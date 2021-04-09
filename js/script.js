4ea2c70fc0578f122f07d24136cd86fe2f0cc186
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-04-09 16:15:37 +0200

    chiamata funzionante

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 const App = new Vue({
     el: "#app",
     data: {
-
-    }
+        currentFilm: [],
+        value: ""
+    },
+    methods: {
+        searchFilm: function(){
+            const Self = this;
+            axios.get('https://api.themoviedb.org/3/search/movie',{
+                params: {
+                    api_key: "fe5a411220156931adf0623172772b2b",
+                    language: "it-IT",
+                    query: Self.value,
+                }
+            })
+            .then(function(response){
+                console.log(response.data);
+            })
+        }
+    },
 })
